1dffa50bd40f290e4f02537b8fe26b261d49a8e4
Pedro Sanzio <phsanzio@outlook.com>
2022-07-13 11:10:29 -0300

    primeiro_commit

---
+++ jogoteca.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+class jogos:
+        def __init__(self, nome, categoria, console):
+                self.nome = nome
+                self.categoria = categoria
+                self.console = console
+
+
+jogo1 = jogos('God of War', 'Ação', 'Playstation')
+jogo2 = jogos('CSGO', 'Ação', 'PC')
+jogo3 = jogos('GTA', 'Ação', 'PC')
+lista = [jogo1, jogo2, jogo3]
+
+@app.route('/inicio')
+def ola():
+    return render_template('lista.html',titulo='Meus Jogos', jogos = lista)
+
+app.run()
